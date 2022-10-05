1fdc227ccfe19cf99eb0d13dff7b2f399f33c360
Tafadzwa <tafadzwamudziviri@gmail.com>
2022-10-05 11:12:11 +0400

    file edit

--- newtheme/header.php
+++ newtheme/header.php
@@ -13,4 +13,33 @@
 
 
 <header class="sticky-top">
+
+
+<div class="container">
+    <?php wp_nav_menu  (
+
+
+        array(
+
+                'theme_location' => 'top-menu' ,
+                'menu_class' => 'navigation'
+
+        )
+
+    );?>
+</div>    
+
 </header>
+
+<div class="container">
+      <h1 class="logo"></h1>
+
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
